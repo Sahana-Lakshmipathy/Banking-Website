a2a520466c4a931be9c031963a45c36fcbdefd18
Sahana-Lakshmipathy <sahana.lp100@gmail.com>
2024-06-24 15:31:57 +0530

    sentry setup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,9 @@
+
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
+import { redirect} from "next/navigation";
 
 
 export default async function RootLayout({
@@ -9,8 +11,11 @@ export default async function RootLayout({
  }: Readonly<{
    children: React.ReactNode;
  }>) {
-
+    
     const loggedIn = await getLoggedInUser();
+    if (!loggedIn) redirect('/sign-in')
+    
+
    return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn}/>
